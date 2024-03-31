806d5e1a313f410251ec0ea481753356547e8f69
mirmakhmudov16 <mr.mirmakhmudov16112008@gmail.com>
2024-03-31 16:47:14 +0500

    #Commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let timerIsRunning = false;
 let intervalID;
-
+let person_hour;
+let person_minute;
+let person_second;
 function formatTime(hour, minute, second) {
     hour = hour < 10 ? '0' + hour : hour;
     minute = minute < 10 ? '0' + minute : minute;
@@ -51,29 +53,43 @@ function showMilliseconds() {
 
 }
 
-function allert(hour, minute) {
+function allert() {
     let person = prompt("oraliq vaqtni kiriting(daqiqa)", "60");
     let now = new Date();
     let hours = now.getHours();
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
-    person_hour = Math.floor(person / 60) + hours
+    person_hour = Math.floor(person / 60) + hours;
     if (person % 60 + minutes != 60) {
-        person_minute = person % 60 + minutes
+        person_minute = person % 60 + minutes;
     }
     else{
-        person_minute=person_minute - 60
-        person_hour= person_hour + 1
+        person_minute=person_minute - 60;
+        person_hour= person_hour + 1;
     }
-    person_second = seconds
+    person_second = seconds;
     let formattedTime = formatTime(person_hour, person_minute, person_second);
-    if (person != null) {
+    if (person !== null && person !== NaN) {
 
         document.getElementById("demo2").innerHTML =
             "Oraliq vaqt " + Math.floor(person/60) + " soat " + person%60 + " minut<br>" + "Tugash vaqti :" + formattedTime;
     }
+    else if (person == null){
+        alert("Error value try again");
+    }
 }
 
+    
+
+function time_up() {
+    if (person_hour == hours && person_minute == minutes && person_second == seconds) {
+        document.getElementById("demo2").style.display = "none";
+        document.getElementById("demo3").style.display = "inline-block";
+    }
+}
+function error_value() {
+  alert("Hello! I am an alert box!");
+}
 
 document.addEventListener('DOMContentLoaded', function () {
     let toggleButton = document.getElementById('btn3');
@@ -111,5 +127,4 @@ function toggleTimer() {
 
 
 showTimer()
-
-
+time_up()
